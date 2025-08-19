fcac4b8f3bfb54bda7a812bc2448592b6390bd33
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2025-08-19 09:57:24 +0300

    py/objint: Add signed param to int.from_bytes().
    Support signed param:
    result = int.from_bytes(bytearray(),
    byteorder='big'|'little', signed=False|True)
    
    Signed-off-by: Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>

--- py/mpz.c
+++ py/mpz.c
@@ -850,36 +850,45 @@ size_t mpz_set_from_str(mpz_t *z, const char *str, size_t len, bool neg, unsigne
     return cur - str;
 }
 
-void mpz_set_from_bytes(mpz_t *z, bool big_endian, size_t len, const byte *buf) {
+void mpz_set_from_bytes(mpz_t *z, bool big_endian, bool is_signed, size_t len, const byte *buf) {
+    if (len == 0) {
+        return;
+    }
+    mpz_dig_t carry = 0;
+    if (is_signed && (buf[big_endian ? 0 : len - 1] & 0x80)) {
+        z->neg = 1;
+        carry = 1;
+    }
     int delta = 1;
     if (big_endian) {
         buf += len - 1;
         delta = -1;
     }
-
     mpz_need_dig(z, (len * 8 + DIG_SIZE - 1) / DIG_SIZE);
-
-    mpz_dig_t d = 0;
+    mpz_dbl_dig_t d = 0;
     int num_bits = 0;
-    z->neg = 0;
-    z->len = 0;
+    mpz_dig_t byte_val;
     while (len) {
-        while (len && num_bits < DIG_SIZE) {
-            d |= *buf << num_bits;
-            num_bits += 8;
-            buf += delta;
-            len--;
+        byte_val = *buf;
+        if (z->neg) {
+            byte_val = (~byte_val & 0xff) + carry;
+            carry = byte_val >> 8;
+            byte_val &= 0xff;
         }
-        z->dig[z->len++] = d & DIG_MASK;
-        // Need this #if because it's C undefined behavior to do: uint32_t >> 32
-        #if DIG_SIZE != 8 && DIG_SIZE != 16 && DIG_SIZE != 32
-        d >>= DIG_SIZE;
-        #else
-        d = 0;
-        #endif
-        num_bits -= DIG_SIZE;
-    }
+        d |= (mpz_dbl_dig_t)byte_val << num_bits;
+        num_bits += 8;
+        buf += delta;
+        len--;
 
+        if (num_bits >= DIG_SIZE) {
+            z->dig[z->len++] = d & DIG_MASK;
+            d >>= DIG_SIZE;
+            num_bits -= DIG_SIZE;
+        }
+    }
+    if (num_bits > 0 || (z->len == 0 && d > 0)) {
+        z->dig[z->len++] = d & DIG_MASK;
+    }
     z->len = mpn_remove_trailing_zeros(z->dig, z->dig + z->len);
 }
 
